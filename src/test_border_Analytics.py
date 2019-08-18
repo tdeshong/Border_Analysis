1477d5ba0acedbf04e88fa96059f22ea14c50da0
tdeshong <36869042+tdeshong@users.noreply.github.com>
2019-08-18 16:40:41 -0400

    Create test_border_Analytics.py

---
+++ src/test_border_Analytics.py
@@ -0,0 +1,34 @@
+import unittest
+import border_analytics
+
+class TestBorderAnalytic(unittest.TestCase):
+
+  def test_readFile(self):
+    ordinary = main.readFile("Border_Crossing_Entry_Data.csv")
+    expected = {'03/01/2019 12:00:00 AM': {'US-Canada Border': {'Truck Containers Full': [6483, 0], 'Trains': [19, 0]}, 'US-Mexico Border': {'Pedestrians': [346158,0]}}, '02/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [172163, 0]}, 'US-Canada Border': {'Truck Containers Empty': [1319, 0]}}, '01/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [56810, 0]}}}
+
+    self.assertDictEqual(ordinary, expected)
+    
+    emptyCsv = main.readFile("Empty.csv")
+    self.assertDictEqual(emptyCsv, {})
+
+    onlyHeader = main.readFile("OnlyHeader.csv")
+    self.assertDictEqual(onlyHeader, {})
+
+    txtFile = main.readFile("Test1.txt")
+    self.assertDictEqual(txtFile, {})
+ 
+ def test_average(self):
+    # consecutive month average
+    dict1 = {'03/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [346158,0]}}, '02/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [172163, 0]}}, '01/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [56810, 0]}}}
+
+    expected1 = {'03/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [346158,114487]}}, '02/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [172163, 56810]}}, '01/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [56810, 0]}}}
+
+    # month have a value of zero
+    dict2 = {'03/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [346158,0]}}, '02/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [172163, 0]}}, '01/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [56810, 0]}}, '04/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [0,0]}}, '05/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [2000,0]}}}
+
+    expected2 = {'03/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [346158,114487]}}, '02/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [172163, 56810]}}, '01/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [56810, 0]}}, '04/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [0,191711]}}, '05/01/2019 12:00:00 AM': {'US-Mexico Border': {'Pedestrians': [2000,143783]}}}
+
+    self.assertDictEqual(main.average(dict1, "US-Mexico Border"), expected1) 
+    
+    self.assertDictEqual(main.average(dict2, "US-Mexico Border"), expected2)
